8380e1617507b8f10ac6f99604cb9693c1da0e93
itsarpit05 <imarpitsachan05@gmail.com>
2025-08-14 20:55:35 +0530

    vite config

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss' // Correct import for Tailwind
 
-// https://vite.dev/config/
 
 export default defineConfig({
-    plugins: [tailwindcss()],
-});
+  plugins: [
+    react(), 
+    
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+})
